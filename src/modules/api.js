e7a8b17da1ffde5009a5809f3a853f48aefb945e
hanqiankun <hanqiankun@gmail.com>
2019-06-08 22:06:43 +0800

    extend APIs

--- src/modules/api.js
+++ src/modules/api.js
@@ -4,11 +4,48 @@ import Constants from './config'
 axios.defaults.withCredentials = true;
 let AcsApi = (function () {
     function AcsApi() { };
+    AcsApi.prototype.get = function(path, page, size, sort){
+        return new Promise((resolve, reject) => {
+            let url = path;
+            if (page || size || sort) {
+                if (url.includes('?')) {
+                    url = url + '&'
+                } else {
+                    url = url + '?';
+                }
+                if (page) {
+                    url = url + 'page=' + page;
+                }
+                if (size) {
+                    url = url + 'size=' + size;
+                }
+                if (sort) {
+                    url = url + 'sort=' + sort;
+                }
+            }
+            axios.get(url).then((res) => {
+                console.log(res);
+                if (res.status === 200) {
+                    resolve(res);
+                    return;
+                }
+                reject(res);
+            }).catch((error) => {
+                console.log('get ' + resource + ' error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    }
     let getResource = function (resource, page, size, sort) {
         return new Promise((resolve, reject) => {
             let url = Constants.API_PREFIX + resource;
             if (page || size || sort) {
-                url = url + '?';
+                if (url.includes('?')) {
+                    url = url + '&'
+                } else {
+                    url = url + '?';
+                }
                 if (page) {
                     url = url + 'page=' + page;
                 }
@@ -50,10 +87,66 @@ let AcsApi = (function () {
             })
         })
     };
+    AcsApi.prototype.addLinks = function (url, links) {
+        return new Promise((resolve, reject) => {
+            let linksText = ''
+            if (links instanceof Array) {
+                for (let i in links) {
+                    linksText += links[i] + '\n'
+                }
+            } else if (links instanceof String) {
+                linksText = links
+            } else {
+                reject({ message: 'param links is invalid' })
+                return
+            }
+            axios.post(url, linksText, { headers: { 'Content-Type': 'text/uri-list' } }).then((res) => {
+                console.log(res);
+                if (res.status === 204) {
+                    resolve(res);
+                    return;
+                }
+                console.log('add links error');
+                reject(res);
+            }).catch((error) => {
+                console.log('add links error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    }
+    AcsApi.prototype.updateLinks = function (url, links) {
+        return new Promise((resolve, reject) => {
+            // let linksText = ''
+            // if (links instanceof Array) {
+            //     for (let i in links) {
+            //         linksText += links[i] + '\n'
+            //     }
+            // } else if (links instanceof String) {
+            //     linksText = links
+            // } else {
+            //     reject({ message: 'param links is invalid' })
+            //     return
+            // }
+            axios.put(url, links, { headers: { 'Content-Type': 'text/uri-list' } }).then((res) => {
+                console.log(res);
+                if (res.status === 204) {
+                    resolve(res);
+                    return;
+                }
+                console.log('add links error');
+                reject(res);
+            }).catch((error) => {
+                console.log('add links error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    }
     AcsApi.prototype.updateResource = function (url, resource) {
         return new Promise((resolve, reject) => {
             axios.put(url, resource).then((res) => {
-                if (res.status == 204) {
+                if (res.status >= 200 && res.status < 300) {
                     resolve(res)
                     return
                 }
@@ -63,10 +156,10 @@ let AcsApi = (function () {
             })
         })
     }
-    AcsApi.prototype.deleteResource = function(url){
+    AcsApi.prototype.deleteResource = function (url) {
         return new Promise((resolve, reject) => {
             axios.delete(url).then((res) => {
-                if(res.status == 204){
+                if (res.status == 204) {
                     resolve(res)
                     return
                 }
@@ -115,11 +208,38 @@ let AcsApi = (function () {
     AcsApi.prototype.getUsers = function (page, size, sort) {
         return getResource('users', page, size, sort)
     };
-    AcsApi.prototype.addUser = function (user) {
-        let url = Constants.API_PREFIX + 'users';
-        axios.post(url, user).then((res) => {
-            console.log(res);
-            return res.data;
+    AcsApi.prototype.addUser = function (name, desc, password, role) {
+        return new Promise((resolve, reject) => {
+            let url = Constants.API_PREFIX + 'api/v1/users';
+            axios.post(url, 'name=' + name + '&desc=' + desc + '&password=' + password + '&role=' + role, { headers: { 'content-type': 'application/x-www-form-urlencoded' } }).then((res) => {
+                console.log(res);
+                if (res.status === 201) {
+                    resolve(res);
+                    return;
+                }
+                reject(res);
+            }).catch((error) => {
+                console.log('add user error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    };
+    AcsApi.prototype.changePwd = function (newPwd, oldPwd) {
+        return new Promise((resolve, reject) => {
+            let url = Constants.API_PREFIX + 'api/v1/user/changePassword';
+            axios.post(url, 'newPwd=' + newPwd + '&oldPwd=' + oldPwd, { headers: { 'content-type': 'application/x-www-form-urlencoded' } }).then((res) => {
+                console.log(res);
+                if (res.status === 201) {
+                    resolve(res);
+                    return;
+                }
+                reject(res);
+            }).catch((error) => {
+                console.log('changePwd error');
+                console.log(error)
+                reject(error);
+            })
         })
     };
     AcsApi.prototype.updateUser = function (user) {
@@ -139,9 +259,49 @@ let AcsApi = (function () {
     AcsApi.prototype.getAlarmPlans = function (page, size, sort) {
         return getResource('alarmPlans', page, size, sort)
     };
-    AcsApi.prototype.addAlarmPlan = function(obj){
+    AcsApi.prototype.getAlarmPlansOfOtherType = function (page, size, sort) {
+        return getResource('alarmPlans/search/findAllOtherType', page, size, sort)
+    };
+    AcsApi.prototype.addAlarmPlan = function (obj) {
         return addResource('alarmPlans', obj)
     }
+    AcsApi.prototype.playPlan = function (alarmPlan) {
+        return new Promise((resolve, reject) => {
+            console.log(alarmPlan._links.self.href)
+            let id = alarmPlan._links.self.href.substring( alarmPlan._links.self.href.lastIndexOf('/') + 1)
+            let url = Constants.API_PREFIX + 'api/v1/alarmPlans/' + id + '/play';
+            axios.post(url).then((res) => {
+                console.log(res);
+                if (res.status === 201) {
+                    resolve(res);
+                    return;
+                }
+                reject(res);
+            }).catch((error) => {
+                console.log('playPlan ' + id + ' error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    };
+    AcsApi.prototype.getPlanStatus = function (alarmPlan) {
+        return new Promise((resolve, reject) => {
+            let id = alarmPlan._links.self.href.substring( alarmPlan._links.self.href.lastIndexOf('/') + 1)
+            let url = Constants.API_PREFIX + 'api/v1/alarmPlans/' + id + '/status';
+            axios.get(url).then((res) => {
+                console.log(res);
+                if (res.status === 200) {
+                    resolve(res);
+                    return;
+                }
+                reject(res);
+            }).catch((error) => {
+                console.log('getPlanStatus ' + id + ' error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    };
     AcsApi.prototype.getAlarmNotices = function (page, size, sort) {
         return getResource('alarmNotices', page, size, sort)
     };
@@ -241,6 +401,38 @@ let AcsApi = (function () {
             })
         })
     };
+    AcsApi.prototype.refreshDevices = function () {
+        return new Promise((resolve, reject) => {
+            axios.post(Constants.API_PREFIX + 'api/v1/alarmDevices/refresh').then((res) => {
+                console.log(res);
+                if (res.status === 200) {
+                    resolve(res);
+                    return;
+                }
+                reject(res);
+            }).catch((error) => {
+                console.log('refreshDevices error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    }
+    AcsApi.prototype.clearDevices = function () {
+        return new Promise((resolve, reject) => {
+            axios.delete(Constants.API_PREFIX + 'api/v1/alarmDevices/clear').then((res) => {
+                console.log(res);
+                if (res.status === 200) {
+                    resolve(res);
+                    return;
+                }
+                reject(res);
+            }).catch((error) => {
+                console.log('refreshDevices error');
+                console.log(error)
+                reject(error);
+            })
+        })
+    }
     AcsApi.prototype.getSystemSettings = function (page, size, sort) {
         let url = Constants.API_PREFIX + 'systemSettings';
         if (page || size || sort) {
