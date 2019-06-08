ed8904cb4aeec7d4070351fa11695416bdcbdee8
hanqiankun <hanqiankun@gmail.com>
2019-06-08 22:09:04 +0800

    add refresh and clear for devices

--- src/views/DeviceManage.vue
+++ src/views/DeviceManage.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <showHead msg="设备管理">
-      <span id="clear_button" class="ver_center extend_button">
+      <span id="clear_button" class="ver_center extend_button" v-on:click="clear">
         <img class="button_img_size" src="../assets/clear.png">清空
       </span>
-      <span id="refresh_button" class="ver_center extend_button">
+      <span id="refresh_button" class="ver_center extend_button" v-on:click="refresh">
         <img class="button_img_size" src="../assets/refresh.png">
         刷新
       </span>
-      <span id="add_button" class="ver_center extend_button">
+      <span id="add_button" class="ver_center extend_button" v-on:click="addDevice">
         <img class="button_img_size" src="../assets/add.png">添加新设备
       </span>
     </showHead>
@@ -20,9 +20,9 @@
 import { mapGetters, mapMutations, mapActions } from "vuex";
 import Head from "@/components/Head.vue";
 import ShowPanel from "@/components/ShowPanel.vue";
-import store from '@/store'
-import AcsApi from '@/modules/api'
-import router from '@/router'
+import store from "@/store";
+import AcsApi from "@/modules/api";
+import router from "@/router";
 
 export default {
   name: "DeviceManage",
@@ -33,30 +33,70 @@ export default {
   data: function() {
     return {
       panelProp: {
-        headers: ['name', 'address', 'ip', 'status'],
-        headerMap: {name: '名称', address: '设备位置', ip: 'IP', status: '状态'},
-        resultList: []//this.alarmDevices._embedded.alarm_device
+        headers: ["name", "address", "ip", "status"],
+        headerMap: {
+          name: "名称",
+          address: "设备位置",
+          ip: "IP",
+          status: "状态"
+        },
+        resultList: [] //this.alarmDevices._embedded.alarm_device
       }
     };
   },
-  beforeRouteEnter (to, from, next) {
+  beforeRouteEnter(to, from, next) {
     // do something before mounted(no this here)
-    next()
+    next();
   },
   computed: {
-    ...mapGetters(['alarmDevices']),
+    ...mapGetters(["alarmDevices"])
   },
-  mounted: function(){
-      this.updateAlarmDevices().then(() => {
-          this.panelProp.resultList = this.alarmDevices._embedded.alarm_device
-          console.log('data AlarmDevices updated')
-      }).catch((error) => {
-          console.error(error)
-          router.push('/logout')
+  mounted: function() {
+    this.updateAlarmDevices()
+      .then(() => {
+        this.panelProp.resultList = this.alarmDevices._embedded.alarm_device;
+        console.log("data AlarmDevices updated");
       })
+      .catch(error => {
+        console.error(error);
+        router.push("/logout");
+      });
   },
   methods: {
-      ...mapActions(['updateAlarmDevices'])
+    ...mapActions(["updateAlarmDevices"]),
+    clear: function() {
+      let api = new AcsApi();
+      api.clearDevices().then(() => {
+        alert("清除成功");
+        this.updateAlarmDevices()
+          .then(() => {
+            this.panelProp.resultList = this.alarmDevices._embedded.alarm_device;
+            console.log("data AlarmDevices updated");
+          })
+          .catch(error => {
+            console.error(error);
+            router.push("/logout");
+          });
+      });
+    },
+    refresh: function() {
+      let api = new AcsApi();
+      api.refreshDevices().then(() => {
+        alert("刷新成功");
+        this.updateAlarmDevices()
+          .then(() => {
+            this.panelProp.resultList = this.alarmDevices._embedded.alarm_device;
+            console.log("data AlarmDevices updated");
+          })
+          .catch(error => {
+            console.error(error);
+            router.push("/logout");
+          });
+      });
+    },
+    addDevice: function() {
+      this.$router.push("deviceManage/addDevice");
+    }
   }
 };
 </script>
